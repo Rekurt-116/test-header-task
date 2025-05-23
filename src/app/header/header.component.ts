3d647b2b68f292256bef78b870205b5b7597a904
Rekurt-116 <sabrlitet@gmail.com>
2025-05-23 21:39:25 +0300

    search-component animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    SearchComponent,
+    NgIf
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  public showSearch = false;
+
+  public toggleSearch() {
+    this.showSearch = !this.showSearch;
+  }
+
+  public hideSearch() {
+    this.showSearch = false;
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, Input, Output, EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-search',
   imports: [],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
+  standalone: true,
 })
 export class SearchComponent {
+  showFilter = false;
 
+  @Output()
+  closeSearch = new EventEmitter<void>();
+
+  @HostListener('document:click', ['$event.target'])
+  onClickoutside(target: HTMLElement) {
+    const clickInside = target.closest('app-search');
+    if(!clickInside) {
+      this.closeSearch.emit();
+    }
+  }
+
+  close() {
+    this.closeSearch.emit();
+  }
 }
