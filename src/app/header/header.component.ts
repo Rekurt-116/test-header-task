2de256330d2852062b0786c1d8dc0e3b8c5efde7
Rekurt-116 <sabrlitet@gmail.com>
2025-05-25 14:59:58 +0300

    layout seach-filter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from '../search/search.component';
 import { NgIf } from '@angular/common';
+import {FilterSearchComponent} from '../filter-search/filter-search.component';
 
 @Component({
   selector: 'app-header',
   imports: [
     SearchComponent,
-    NgIf
+    NgIf,
+    FilterSearchComponent
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  standalone: true,
 })
 export class HeaderComponent {
 
