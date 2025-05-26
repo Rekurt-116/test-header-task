3a46ac12850a758d34ecee73730536f5b5e9d60a
Rekurt-116 <sabrlitet@gmail.com>
2025-05-26 14:35:04 +0300

    initial-full-project

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
+import { NgIf } from '@angular/common';
+import {FilterSearchComponent} from '../filter-search/filter-search.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    SearchComponent,
+    NgIf,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  standalone: true,
 })
 export class HeaderComponent {
 
+  public showSearch = false;
+
+  public toggleSearch() {
+    this.showSearch = !this.showSearch;
+  }
+
+  public hideSearch() {
+    this.showSearch = false;
+  }
 }
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,41 @@
+import {Component, HostListener, Output, EventEmitter} from '@angular/core';
+import { FilterSearchComponent } from '../filter-search/filter-search.component';
+import { NgIf } from '@angular/common';
+
+
+@Component({
+  selector: 'app-search',
+  imports: [
+    FilterSearchComponent,
+    NgIf 
+  ],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+  standalone: true,
+})
+export class SearchComponent {
+  showFilter = false;
+
+  @Output()
+  closeSearch = new EventEmitter<void>();
+
+  @HostListener('document:click', ['$event.target'])
+  onClickoutside(target: HTMLElement) {
+    const clickInside = target.closest('app-search');
+    if(!clickInside) {
+      this.closeSearch.emit();
+    }
+  }
+
+  close() {
+    this.closeSearch.emit();
+  }
+
+  public toggleFilter() {
+    this.showFilter = !this.showFilter;
+  }
+
+  public hideFilter() {
+    this.showFilter = false;
+  }
+}
